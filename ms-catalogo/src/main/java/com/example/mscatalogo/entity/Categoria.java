bff70316e9f7889244727dbcc5f997eddea042ee
JoseManuel159 <josexx741xx@outlook.es>
2025-03-24 21:26:08 -0500

    Primer commit

---
+++ ms-catalogo/src/main/java/com/example/mscatalogo/entity/Categoria.java
@@ -0,0 +1,45 @@
+package com.example.mscatalogo.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+
+    // Constructor vacío
+    public Categoria() {
+    }
+
+    // Constructor con parámetros
+    public Categoria(Integer id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    // Getters y Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
